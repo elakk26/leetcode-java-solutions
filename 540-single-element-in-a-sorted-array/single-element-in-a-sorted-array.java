7c5edd992182e8eb21662f27a6b41ece0ec10477
Elakkiya <26elakkiya@gmail.com>
2026-06-17 17:52:54 +0530

    Time: 1 ms (24.15%) | Memory: 52.8 MB (76.00%) - LeetSync

---
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int sum=0;
+        for(int i:nums)
+        {
+            sum=sum^i;
+        }
+        return sum;
+    }
+}
