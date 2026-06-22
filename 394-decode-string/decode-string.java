4e998bca1f519816452e80c3e53ab43421f50950
Elakkiya <26elakkiya@gmail.com>
2026-06-22 20:30:48 +0530

    Time: 1 ms (85.52%) | Memory: 43.1 MB (14.57%) - LeetSync

--- 394-decode-string/decode-string.java
+++ 394-decode-string/decode-string.java
@@ -1,37 +1,31 @@
 class Solution {
     public String decodeString(String s) {
-        Stack<Integer> count=new Stack<>();
-        Stack<String> str=new Stack<>();
-        String ans="";
-        int num=0;
-        for(char ch:s.toCharArray())
-        {
-            if(ch>='0'&&ch<='9')
-            {
-                num=num*10+(ch-'0');
-            }
-            else if(ch=='[')
-            {
-                count.push(num);
-                str.push(ans);
-                num=0;
-                ans="";
-            }
-            else if(ch>='a'&&ch<='z')
-            {
-                ans+=ch;
-            }
-            else if(ch==']')
-            {
-                int times=count.pop();
-                String old=str.pop();
-                String temp="";
-                for(int i=0;i<times;i++)
-                {
-                    temp+=ans;
+        Stack<String> st = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            if (ch != ']') {
+                st.push(ch + "");
+            } else {
+                String str = "";
+                while (!st.peek().equals("[")) {
+                    str = st.pop() + str;
                 }
-                ans=old+temp;
+                st.pop();
+                String num = "";
+                while (!st.isEmpty() &&
+                       Character.isDigit(st.peek().charAt(0))) {
+                    num = st.pop() + num;
+                }
+                int k = Integer.parseInt(num);
+                String temp = "";
+                for (int i = 0; i < k; i++) {
+                    temp += str;
+                }
+                st.push(temp);
             }
+        }
+        String ans = "";
+        while (!st.isEmpty()) {
+            ans = st.pop() + ans;
         }
         return ans;
     }
