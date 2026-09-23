cebeb8c2f98df263bbe2fb2d94f360f93742afaa
Elakkiya <26elakkiya@gmail.com>
2026-09-23 22:50:58 +0530

    Time: 1 ms (83.67%) | Memory: 42.4 MB (98.28%) - LeetSync

--- 394-decode-string/decode-string.java
+++ 394-decode-string/decode-string.java
@@ -1,32 +1,47 @@
 class Solution {
     public String decodeString(String s) {
-        Stack<String> st = new Stack<>();
-        for (char ch : s.toCharArray()) {
-            if (ch != ']') {
-                st.push(ch + "");
-            } else {
-                String str = "";
-                while (!st.peek().equals("[")) {
-                    str = st.pop() + str;
-                }
-                st.pop();
-                String num = "";
-                while (!st.isEmpty() &&
-                       Character.isDigit(st.peek().charAt(0))) {
-                    num = st.pop() + num;
-                }
-                int k = Integer.parseInt(num);
-                String temp = "";
-                for (int i = 0; i < k; i++) {
-                    temp += str;
-                }
-                st.push(temp);
+        Stack<String> st=new Stack<>();
+        Stack<Integer> stn=new Stack<>();
+
+    int k=0;
+    String cur="";
+        for(char c:s.toCharArray())
+        {
+            if(Character.isDigit(c))
+            {
+                k=k*10 +(c-'0');
             }
+           else if(c=='[')
+           {
+            stn.push(k);
+            k=0;
+            st.push(c+"");
+           }
+
+           else if(c==']')
+           {
+            String temp="";
+            while(!st.isEmpty()&& !st.peek().equals("["))
+                temp=st.pop()+temp;
+
+            st.pop();
+            int count=stn.pop();
+            String dub="";
+            for(int i=0;i<count;i++)
+            {
+            dub+=temp;
+            }
+            st.push(dub);
+           }
+
+           else
+           st.push(c+"");
         }
-        String ans = "";
+        String result = "";
         while (!st.isEmpty()) {
-            ans = st.pop() + ans;
+            result = st.pop() + result;
         }
-        return ans;
+
+        return result;
     }
 }
